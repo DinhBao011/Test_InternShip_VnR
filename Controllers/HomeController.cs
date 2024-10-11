ea2ed6c0dc8f3e5194c86665807da75deec9b66a
DinhBao011 <baooccho000@gmail.com>
2024-10-12 01:39:39 +0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test_VnR.Models;
+
+namespace Test_VnR.Controllers
+{
+    public class HomeController : Controller
+    {
+        VNR_InternShipEntities db = new VNR_InternShipEntities();
+        public ActionResult Index()
+        {
+            var courseList = db.KhoaHocs.ToList();
+            return View(courseList);
+        }
+
+        public ActionResult choose_Frontend_Option(int id)
+        {
+            ViewBag.KhoaHoc = db.KhoaHocs.FirstOrDefault(c => c.ID == id).TenKhoaHoc;
+            var subjectList = db.MonHocs.Where(p => p.KhoaHocID == id).ToList();
+            return View(subjectList);
+        }
+
+        public ActionResult choose_BackEnd_Option(int id)
+        {
+            ViewBag.KhoaHoc = db.KhoaHocs.FirstOrDefault(c => c.ID == id).TenKhoaHoc;
+            var subjectList = db.MonHocs.Where(p => p.KhoaHocID == id).ToList();
+            return View(subjectList);
+        }
+
+        public ActionResult choose_DotnetCore_Option(int id)
+        {
+            ViewBag.KhoaHoc = db.KhoaHocs.FirstOrDefault(c => c.ID == id).TenKhoaHoc;
+            var subjectList = db.MonHocs.Where(p => p.KhoaHocID == id).ToList();
+            return View(subjectList);
+        }
+
+    }
+}
